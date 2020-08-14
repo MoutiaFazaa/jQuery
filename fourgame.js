41e585c29e393ee55534ec699a69e991ee3a6389
MoutiaFazaa <fazaamouty@gmail.com>
2020-08-14 15:16:11 -0400

    Four Game LOGIC

---
+++ fourgame.js
@@ -0,0 +1,100 @@
+var player1=prompt("Player One: Enter your name, you will be blue");
+var player1Color="rgb(86,151,255)";
+
+var player2=prompt("Player Two: Enter your name, you will be red");
+var player2Color="rgb(237,45,73)";
+
+var game_on=true;
+var table= $("table tr");
+
+function reportWin(rowNum,colNum){
+    console.log("You won starting at this row,col");
+    console.log(rowNum);
+    console.log(colNum);
+}
+
+function changeColor(rowIndex,colIndex,color){
+    return table.eq(rowIndex).find("td").eq(colIndex).find('button').css("backgroung-color",color);
+}
+
+function returnColor(rowIndex,colIndex){
+    return table.eq(rowIndex).find("td").eq(colIndex).find('button').css("backgroung-color");
+}
+
+function checkBottom(colIndex){
+    var colorReport= returnColor(5,colIndex);
+    for(var row=5; row> -1; row--){
+        colorReport=returnColor(row,colIndex);
+        if(colorReport==="rgb(128,128,128"){
+            return row
+        }
+    }
+}
+
+function colorMatchCheck(one,two,three,four){
+    return(one===two && one===three && one === four && one!=="rgb(128,128,128)" && one!==undefined)
+}
+
+//Check for Horizontal
+function HorizontalWinCheck(){
+    for(var row =0; row< 6; row++){
+        for(var col=0; col<4; col++){
+            if(colorMatchCheck(returnColor(row,col), returnColor(row,col+1) ,returnColor(row,col+2), returnColor(row,col+3))) {
+                console.log("horiz");
+                reportWin(row,col);
+                return true;
+            }else{
+                continue;
+            }
+        }
+    }
+}
+
+//Check for Vertical
+function VerticalWinCheck(){
+    for(var col =0; col< 7; col++){
+        for(var row=0; row<3; row++){
+            if(colorMatchCheck(returnColor(row,col), returnColor(row+1,col) ,returnColor(row+2,col), returnColor(row+3,col))) {
+                console.log("vertical");
+                reportWin(row,col);
+                return true;
+            }else{
+                continue;
+            }
+        }
+    }
+}
+// Check for Diagonal Wins
+function diagonalWinCheck() {
+
+    for (var col = 0; col < 5; col++) {
+  
+      for (var row = 0; row < 7; row++) {
+  
+        if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col+1) ,returnColor(row+2,col+2), returnColor(row+3,col+3))) {
+  
+          console.log('diag');
+  
+          reportWin(row,col);
+  
+          return true;
+  
+        }else if (colorMatchCheck(returnColor(row,col), returnColor(row-1,col+1) ,returnColor(row-2,col+2), returnColor(row-3,col+3))) {
+  
+          console.log('diag');
+  
+          reportWin(row,col);
+  
+          return true;
+  
+        }else {
+  
+          continue;
+  
+        }
+  
+      }
+  
+    }
+  
+  }
