30727b84a8eacfc9198bbf4a67716efdf1804e15
MoutiaFazaa <fazaamouty@gmail.com>
2020-08-16 13:24:43 -0400

    FOUR GAME DONE

--- fourgame.js
+++ fourgame.js
@@ -1,44 +1,44 @@
-var player1=prompt("Player One: Enter your name, you will be blue");
-var player1Color='rgb(86, 151, 255)';
+var player1 = prompt("Player One: Enter your name, you will be blue");
+var player1Color = 'rgb(86, 151, 255)';
 
-var player2=prompt("Player Two: Enter your name, you will be red");
-var player2Color='rgb(237, 45, 73)';
+var player2 = prompt("Player Two: Enter your name, you will be red");
+var player2Color = 'rgb(237, 45, 73)';
 
-var game_on=true;
+var game_on = true;
 var table= $("table tr");
-
-function reportWin(rowNum,colNum){
+//___________________________
+function reportWin(rowNum,colNum) {
     console.log("You won starting at this row,col");
     console.log(rowNum);
     console.log(colNum);
 }
 
-function changeColor(rowIndex,colIndex,color){
-    return table.eq(rowIndex).find("td").eq(colIndex).find('button').css("backgroung-color",color);
+function changeColor(rowIndex,colIndex,color) {
+    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+}
+//___________________________
+function returnColor(rowIndex,colIndex) {
+    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css("background-color");
 }
 
-function returnColor(rowIndex,colIndex){
-    return table.eq(rowIndex).find("td").eq(colIndex).find('button').css("backgroung-color");
-}
-
-function checkBottom(colIndex){
+function checkBottom(colIndex) {
     var colorReport= returnColor(5,colIndex);
-    for(var row=5; row> -1; row--){
+    for(var row=5; row > -1; row--) {
         colorReport=returnColor(row,colIndex);
-        if(colorReport==="rgb(128,128,128"){
+        if(colorReport==='rgb(128,128,128') {
             return row
         }
     }
 }
-
+//_____________________________
 function colorMatchCheck(one,two,three,four){
-    return(one===two && one===three && one === four && one!=="rgb(128,128,128)" && one!==undefined)
+    return(one===two && one===three && one === four && one!== 'rgb(128,128,128)' && one!==undefined);
 }
 
 //Check for Horizontal
-function HorizontalWinCheck(){
-    for(var row =0; row< 6; row++){
-        for(var col=0; col<4; col++){
+function horizontalWinCheck(){
+    for(var row =0; row < 6; row++) {
+        for(var col=0; col < 4; col++){
             if(colorMatchCheck(returnColor(row,col), returnColor(row,col+1) ,returnColor(row,col+2), returnColor(row,col+3))) {
                 console.log("horiz");
                 reportWin(row,col);
@@ -49,9 +49,9 @@ function HorizontalWinCheck(){
         }
     }
 }
-
+//___________________________
 //Check for Vertical
-function VerticalWinCheck(){
+function verticalWinCheck(){
     for(var col =0; col< 7; col++){
         for(var row=0; row<3; row++){
             if(colorMatchCheck(returnColor(row,col), returnColor(row+1,col) ,returnColor(row+2,col), returnColor(row+3,col))) {
@@ -99,20 +99,34 @@ function diagonalWinCheck() {
   
   }
 
+  // Game End
+function gameEnd(winningPlayer) {
+    for (var col = 0; col < 7; col++) {
+      for (var row = 0; row < 7; row++) {
+        $('h3').fadeOut('fast');
+        $('h2').fadeOut('fast');
+        $('h1').text(winningPlayer+" has won! Refresh your browser to play again!").css("fontSize", "50px")
+      }
+    }
+  }
+
 //start with player 1
 var currentPlayer=1;
 var currentName=player1;
 var currentColor=player1Color;
- $("h3").text(player1+" it is your turn, pick a column to drop in!")
- $('.board button').on("click", function(){
-     var col = $(this).closest('td').index();
+
+
+ $('h3').text(player1+" it is your turn, pick a column to drop in!");
+ $('.board button').on('click', function(){
+     var col = $(this).closest("td").index();
      var bottomAvail=checkBottom(col);
      changeColor(bottomAvail,col,currentColor);
-     if(HorizontalWinCheck() || VerticalWinCheck()|| diagonalWinCheck()){
-         $('h1').text(currentName+" you have won!");
-         $('h3').fadeOut('fast');
+     if(horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()){
+        gameEnd(currentName);
      }
-     currentPlayer =currentPlayer* -1;
+
+
+     currentPlayer = currentPlayer * -1 ;
 
      if(currentPlayer===1){
          currentName=player1;
